01465efcea72e3174b6232bc9e30832b09593903
omkar7860 <omkar078655@gmail.com>
2023-09-16 19:10:54 +0530

    Added card header in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import Card from './components/Card/Card.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
